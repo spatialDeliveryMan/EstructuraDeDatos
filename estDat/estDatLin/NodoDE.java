85d9e69017ae2cebe4b16d750fe646841d51becb
spatialDeliveryMan <luisanghelopena@gmail.com>
2019-05-07 11:43:24 -0400

    ola wapaç

---
+++ estDat/estDatLin/NodoDE.java
@@ -0,0 +1,15 @@
+package estDat.estDatLin;
+
+
+/**
+ * Write a description of class NodoDE here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class NodoDE<T>{
+    private NodoDE<T> ant;
+    private T         dato;
+    private NodoDE<T> suc;
+    
+}
